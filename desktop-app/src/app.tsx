7b294691cbcabe6a2033f7d6358373f4b0536fcf
DrDestroydo <woodsy867@gmail.com>
2022-02-17 21:07:21 +0800

    Adding initial files

---
+++ desktop-app/src/app.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Desktop App</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.body);
